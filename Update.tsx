bb32604f0d171acc2bc9dd0478a70f82209187be
z3fang <eodiopq@gmail.com>
2023-11-15 14:40:19 +0000

    chore(bot): 🙏 auto commit

--- Update.tsx
+++ Update.tsx
@@ -3750,3 +3750,7 @@ curl: (22) The requested URL returned error: 502
                                  Dload  Upload   Total   Spent    Left  Speed
   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0  0   182    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0
 curl: (22) The requested URL returned error: 502
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0    223      0 --:--:-- --:--:-- --:--:--   223
+{"code":"InvalidParameters","error":"not found","data":null}
