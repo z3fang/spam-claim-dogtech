54b7c0b70c513b458810a6474e225350529bddfa
z3fang <eodiopq@gmail.com>
2023-11-15 23:50:07 +0000

    chore(bot): 🟩 auto commit

--- Update.tsx
+++ Update.tsx
@@ -10774,3 +10774,7 @@ curl: (22) The requested URL returned error: 408
                                  Dload  Upload   Total   Spent    Left  Speed
   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0   1374      0 --:--:-- --:--:-- --:--:--  1395
 {"code":"InvalidParameters","error":"not found","data":null}
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0   1204      0 --:--:-- --:--:-- --:--:--  1224
+{"code":"InvalidParameters","error":"not found","data":null}
